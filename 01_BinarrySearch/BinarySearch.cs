27f1fecc5932860296afcde71877552bb474e8c8
Vladimir <bibubl@yandex.com>
2021-03-16 23:45:55 +0300

    01 - Бинарный поиск

---
+++ 01_BinarrySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_BinarySearch
+{
+    class BinarySearch
+    {
+        public static void Search(int[] arr, int target)
+        {
+            int low = arr[0];
+            int high = arr[arr.Length - 1];
+
+            while (low < high)
+            {
+                int mid = (low + high)/2;
+                int sought = arr[mid];
+                if (target < sought)
+                {
+                    high = arr[mid - 1];
+                }
+                else if (target > sought)
+                {
+                    low = arr[mid - 1];
+                }
+                else if(target == sought)
+                {
+                    Console.WriteLine($"Найдено в ячейке {mid}");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- 01_BinarrySearch/Program.cs
+++ 01_BinarrySearch/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace _01_BinarrySearch
+namespace _01_BinarySearch
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i+1;
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            BinarySearch.Search(arr, 17);
         }
     }
 }
