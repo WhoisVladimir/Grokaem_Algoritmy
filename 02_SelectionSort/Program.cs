de60e01e3bb3aa04c4e1469541e7944a6d00919c
Vladimir <bibubl@yandex.com>
2021-03-17 14:34:47 +0300

    02 - сортировка выбором

---
+++ 02_SelectionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nonSorted = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                nonSorted.Add(random.Next(100));
+            }
+
+            foreach (var item in nonSorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            SelectionSort.SelSort(nonSorted);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02_SelectionSort/SelectionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_SelectionSort
+{
+    class SelectionSort
+    {
+
+        public static void SelSort(List<int> list)
+        {
+            int smallest;
+            int smallestIndex;
+            List<int> sortedList = new List<int>();
+            for (int i = 0; list.Count > 0; i++)
+            {
+                smallest = list[0];
+                smallestIndex = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[j] < smallest)
+                    {
+                        smallest = list[j];
+                        smallestIndex = j;
+                    }
+                }
+                sortedList.Add(smallest);
+                list. RemoveAt(smallestIndex);
+            }
+
+            Console.WriteLine("Done:");
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
