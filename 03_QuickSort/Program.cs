a36513f391ea11c33145adf7071b91d791076cc3
Vladimir <bibubl@yandex.com>
2021-03-17 20:09:19 +0300

    03 - быстрая сортировка

---
+++ 03_QuickSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nonSorted = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                nonSorted.Add(random.Next(100));
+            }
+
+            foreach (var item in nonSorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            nonSorted = QuickSort.QSort(nonSorted);
+
+            Console.WriteLine("\nDone:\n");
+            foreach (var item in nonSorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 03_QuickSort/QuickSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_QuickSort
+{
+    class QuickSort
+    {
+        public static List<int> QSort(List<int> list)
+        {
+            if (list.Count < 2) return list;
+            else
+            {
+                int pivot = list[0];
+                List<int> sorted = new List<int>();
+                List<int> less = new List<int>();
+                List<int> greater = new List<int>();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] < pivot) less.Add(list[i]);
+                    else if(list[i] > pivot) greater.Add(list[i]);
+                }
+                less = QSort(less);
+                greater = QSort(greater);
+                sorted.AddRange(less);
+                sorted.Add(pivot);
+                sorted.AddRange(greater);
+                return sorted;
+            }
+        }
+    }
+}
